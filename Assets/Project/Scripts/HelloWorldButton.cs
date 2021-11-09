68a223e49c6e9172e705bf1677b35c491de089db
edom18 <edo.m18@gmail.com>
2021-11-10 01:11:13 +0900

    feat: Added some feature to recognize subscription force.

--- Assets/Project/Scripts/HelloWorldButton.cs
+++ Assets/Project/Scripts/HelloWorldButton.cs
@@ -5,6 +5,7 @@ using GraphQL;
 using UnityEngine;
 using GraphQL.Client.Http;
 using GraphQL.Client.Serializer.Newtonsoft;
+using Newtonsoft.Json;
 
 public class HelloWorldButton : MonoBehaviour
 {
@@ -67,14 +68,15 @@ public class HelloWorldButton : MonoBehaviour
     {
         var headerStr = @" { ""host"": ""zrzxcskrlfcsxmvuusj2knj6iu.appsync-api.us-east-2.amazonaws.com"", ""x-api-key"": ""da2-jdorukduzzdbbgrfubngjzi24a"" }";
         var header = Convert.ToBase64String(Encoding.UTF8.GetBytes(headerStr));
-        // var graphQLClient = new GraphQLHttpClient($"https://zrzxcskrlfcsxmvuusj2knj6iu.appsync-api.us-east-2.amazonaws.com/graphql?header={header}&payload=e30=", new NewtonsoftJsonSerializer());
-        var graphQLClient = new GraphQLHttpClient($"https://zrzxcskrlfcsxmvuusj2knj6iu.appsync-realtime-api.us-east-2.amazonaws.com/graphql?header={header}&payload=e30=", new NewtonsoftJsonSerializer());
-        // graphQLClient.HttpClient.DefaultRequestHeaders.Add("host", "zrzxcskrlfcsxmvuusj2knj6iu.appsync-api.us-east-2.amazonaws.com");
-        // graphQLClient.HttpClient.DefaultRequestHeaders.Add("x-api-key", API_KEY);
-        // graphQLClient.Options.WebSocketEndPoint = new Uri($"{WSS_API_URL}?header={header}&payload=e30=");
-        // graphQLClient.HttpClient.DefaultRequestHeaders.Add("x-api-key", API_KEY);
+        var graphQLClient = new GraphQLHttpClient($"wss://zrzxcskrlfcsxmvuusj2knj6iu.appsync-realtime-api.us-east-2.amazonaws.com/graphql?header={header}&payload=e30=", new NewtonsoftJsonSerializer());
+        graphQLClient.Options.UseWebSocketForQueriesAndMutations = true;
+        graphQLClient.Options.WebSocketEndPoint = new Uri($"wss://zrzxcskrlfcsxmvuusj2knj6iu.appsync-realtime-api.us-east-2.amazonaws.com/graphql?header={header}&payload=e30=");
 
-        var request = new GraphQLHttpRequest
+        await graphQLClient.InitializeWebsocketConnection();
+
+        Debug.Log("Initialized a web scoket connection.");
+
+        var request = new GraphQLRequest
         {
             Query = @"
             subscription MySubscription {
@@ -85,7 +87,15 @@ public class HelloWorldButton : MonoBehaviour
             }",
         };
 
-        var subscriptionStream = graphQLClient.CreateSubscriptionStream<SubscriptionResponse>(request, ex =>
+        var request2 = new GraphQLRequest
+        {
+            ["data"] = JsonConvert.SerializeObject(request),
+            ["extensions"] = new {
+                authorization = header,
+            }
+        };
+
+        var subscriptionStream = graphQLClient.CreateSubscriptionStream<SubscriptionResponse>(request2, ex =>
         {
             Debug.Log(ex);
         });
